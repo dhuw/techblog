2450b7383fdaa97695574ac0ce6713d09a821aab
dhuw <hazlewood.dyl@gmail.com>
2022-09-23 01:34:39 -0700

    4th try

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 55751;
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
@@ -28,7 +28,7 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
+// Inform Express.js on which template engine to use 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
